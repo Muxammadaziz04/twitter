8b17f44bc4b7160c06fdb9ab4c0318df11244fa9
muxammadziz <muxammadazizramziddinov@gmail.com>
2022-03-14 15:32:24 +0500

    link

--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import "./home.scss"
 import asideLogo from "../../Assets/svg/twitter-aside-logo.svg"
 import home from "../../Assets/svg/home.svg"
